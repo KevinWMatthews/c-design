aaaa7238018e9834b309a0fd144a0d7825f27ebe
Kevin W Matthews <kevinwmatthews@gmail.com>
2020-12-24 10:13:11 -0800

    Refactor: Rename variables in blocking eventfd example

--- my_eventfd/examples/eventfd_nonblocking.c
+++ my_eventfd/examples/eventfd_nonblocking.c
@@ -16,23 +16,23 @@ int main(void)
     int efd = eventfd(0, EFD_NONBLOCK);
     assert(efd != -1);
 
-    uint64_t value = 3;
-    printf("Writing to eventfd: '%" PRIu64 "'\n", value);
-    ssize_t n_written = write(efd, &value, sizeof(value));
+    uint64_t write_value = 3;
+    printf("Writing to eventfd: '%" PRIu64 "'\n", write_value);
+    ssize_t n_written = write(efd, &write_value, sizeof(write_value));
     assert(n_written != -1);
-    assert(n_written == sizeof(value));
+    assert(n_written == sizeof(write_value));
 
     printf("Reading from eventfd...\n");
-    value = 0;
-    ssize_t n_read = read(efd, &value, sizeof(value));
+    uint64_t read_value = 0;
+    ssize_t n_read = read(efd, &read_value, sizeof(read_value));
     assert(n_read != -1);
-    assert(n_read == sizeof(value));
-    printf("Read from eventfd: '%" PRIu64 "'\n", value);
+    assert(n_read == sizeof(read_value));
+    printf("Read from eventfd: '%" PRIu64 "'\n", read_value);
 
     // If the eventfd's value is 0, read() will fail
     printf("Read would block...\n");
-    value = 0;
-    n_read = read(efd, &value, sizeof(value));
+    read_value = 0;
+    n_read = read(efd, &read_value, sizeof(read_value));
     assert(n_read == -1);
     assert(errno == EAGAIN || errno == EWOULDBLOCK);
     printf("Value not read\n");
