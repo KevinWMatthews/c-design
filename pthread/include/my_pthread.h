6374c665f4c0a1e7603a0cce97f35a92f06695f0
Kevin W Matthews <kevinwmatthews@gmail.com>
2021-02-11 10:25:24 -0800

    Expand pthread wrapper API

--- pthread/include/my_pthread.h
+++ pthread/include/my_pthread.h
@@ -9,15 +9,85 @@
 
 #include <pthread.h>
 
-typedef void *(*my_pthread_start_routine_t )(void *);
+/**
+ * @brief Prototype for a pthread function.
+ */
+typedef void* (* my_pthread_start_routine_t)(void*);
 
+/**
+ * @brief Create a thread.
+ *
+ * The resulting thread must be either joined or detached.
+ *
+ * @param start_routine
+ * @param arg
+ * @param attr
+ * @return
+ */
 pthread_t my_pthread_create(my_pthread_start_routine_t start_routine,
-    void *arg,
-    const pthread_attr_t *attr);
-pthread_t my_pthread_create_noattr(my_pthread_start_routine_t start_routine, void *arg);
+    const pthread_attr_t* attr,
+    void* arg);
+
+/**
+ * @brief Create a thread with default attributes.
+ *
+ * The resulting thread must be either joined or detached.
+ *
+ * @param start_routine
+ * @param arg
+ * @return
+ */
+pthread_t my_pthread_create_noattr(my_pthread_start_routine_t start_routine, void* arg);
+
+/**
+ * @brief Create a thread with default attributes and no arguments.
+ *
+ * The resulting thread must be either joined or detached.
+ *
+ * @param start_routine
+ * @return
+ */
 pthread_t my_pthread_create_noarg(my_pthread_start_routine_t start_routine);
 
-void *my_pthread_join(pthread_t handle);
+/**
+ * @brief Create and detach a thread.
+ * @param start_routine
+ * @param arg
+ * @param attr
+ */
+void my_pthread_spawn(my_pthread_start_routine_t start_routine,
+    const pthread_attr_t* attr,
+    void* arg);
+
+/**
+ * @brief Create and detach a thread with default attributes and no arguments.
+ * @param start_routine
+ * @param arg
+ */
+void my_pthread_spawn_noattr(my_pthread_start_routine_t start_routine, void* arg);
+
+/**
+ * @brief Create and detach a thread with default attributes and no arguments.
+ * @param start_routine
+ */
+void my_pthread_spawn_noarg(my_pthread_start_routine_t start_routine);
+
+/**
+ * @brief Detach a thread.
+ */
+void my_pthread_detach(pthread_t handle);
+
+/**
+ * @brief Join a thread and return the thread's return value.
+ * @param handle
+ * @return
+ */
+void* my_pthread_join(pthread_t handle);
+
+/**
+ * @brief Join a thread and ignore the thread's return value.
+ * @param handle
+ */
 void my_pthread_join_noarg(pthread_t handle);
 
 #endif
--- pthread/src/my_pthread.c
+++ pthread/src/my_pthread.c
@@ -5,8 +5,8 @@
 #include "my_pthread.h"
 
 pthread_t my_pthread_create(my_pthread_start_routine_t start_routine,
-    void *arg,
-    const pthread_attr_t *attr)
+    const pthread_attr_t* attr,
+    void* arg)
 {
     pthread_t handle;
     int err = pthread_create(&handle, attr, start_routine, arg);
@@ -14,25 +14,43 @@ pthread_t my_pthread_create(my_pthread_start_routine_t start_routine,
     return handle;
 }
 
-pthread_t my_pthread_create_noattr(my_pthread_start_routine_t start_routine, void *arg)
+pthread_t my_pthread_create_noattr(my_pthread_start_routine_t start_routine, void* arg)
 {
-    pthread_t handle;
-    int err = pthread_create(&handle, NULL, start_routine, arg);
-    assert(!err);
-    return handle;
+    return my_pthread_create(start_routine, NULL, arg);
 }
 
 pthread_t my_pthread_create_noarg(my_pthread_start_routine_t start_routine)
 {
-    pthread_t handle;
-    int err = pthread_create(&handle, NULL, start_routine, NULL);
-    assert(!err);
-    return handle;
+    return my_pthread_create(start_routine, NULL, NULL);
 }
 
-void *my_pthread_join(pthread_t handle)
+void my_pthread_spawn(my_pthread_start_routine_t start_routine,
+    const pthread_attr_t* attr,
+    void* arg)
 {
-    void *retval = NULL;
+    pthread_t handle = my_pthread_create(start_routine, attr, arg);
+    pthread_detach(handle);
+}
+
+void my_pthread_spawn_noattr(my_pthread_start_routine_t start_routine, void* arg)
+{
+    my_pthread_spawn(start_routine, NULL, arg);
+}
+
+void my_pthread_spawn_noarg(my_pthread_start_routine_t start_routine)
+{
+    my_pthread_spawn(start_routine, NULL, NULL);
+}
+
+void my_pthread_detach(pthread_t handle)
+{
+    int err = pthread_detach(handle);
+    assert(!err);
+}
+
+void* my_pthread_join(pthread_t handle)
+{
+    void* retval = NULL;
     int err = pthread_join(handle, &retval);
     assert(!err);
     return retval;
@@ -40,6 +58,5 @@ void *my_pthread_join(pthread_t handle)
 
 void my_pthread_join_noarg(pthread_t handle)
 {
-    int err = pthread_join(handle, NULL);
-    assert(!err);
+    my_pthread_join(handle);
 }
